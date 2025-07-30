b4541ca981cd031adfc8d5821f2e9bc3bd6ed198
José Coca <josecoca.cn@gmail.com>
2025-07-29 20:27:43 -0500

    DB

--- db_senac.sql
+++ db_senac.sql
@@ -7,7 +7,7 @@ CREATE TABLE pessoa (
 	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(120) NOT NULL,
 	cpf CHAR(14) NOT NULL,
-	email VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
 	senha VARCHAR(64) NOT NULL,
 	telefone VARCHAR(20) NOT NULL,
 	sexo ENUM('Masculino', 'Feminino') NOT NULL,
@@ -96,8 +96,7 @@ SELECT
 FROM pessoa
 WHERE
 	sexo = 'Feminino';
-*/
-/*	
+
 SELECT
 	sexo,
 	YEAR(data_nascimento) AS ano,
@@ -113,26 +112,21 @@ HAVING
 ORDER BY
 	ano DESC,
 	sexo ASC;
-*/
-/*	
+	
 SELECT
 	nome,
 	UPPER(nome) AS nome_maiusculo,
 	LOWER(nome) AS nome_minusculo,
 	REPLACE(REPLACE(LOWER(nome), 'a', '4'), 'o', '0') AS nome_sem_espaco
 FROM pessoa;
-*/
 
-/*
 CREATE TABLE curso (
 	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(80) NOT NULL,
 	carga_horaria INT(9) NOT NULL,
 	modalidade ENUM('Presencial', 'Online', 'Mista') NOT NULL DEFAULT 'Presencial' 
 );
-*/
 
-/*
 CREATE TABLE matricula (
 	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_pessoa INT(10) NOT NULL,
@@ -142,7 +136,6 @@ CREATE TABLE matricula (
 	data_inicio DATE NOT NULL,
 	data_termino DATE NOT NULL
 );
-*/
 
 CREATE TABLE telefone (
 	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -150,3 +143,44 @@ CREATE TABLE telefone (
 	id_pessoa INT(10) NOT NULL,
 	FOREIGN KEY (id_pessoa) REFERENCES pessoa (id)
 );
+
+INSERT INTO curso (
+    nome, carga_horaria, modalidade
+) VALUES 
+('Programador Web', 240, 'Presencial'),
+('Sushiman', 180, 'Presencial'),
+('Mecânico de Peugeot', 360, 'Online');
+
+INSERT INTO matricula (
+    id_pessoa, id_curso, data_inicio, data_termino
+) VALUES 
+(1, 2, '2025-07-01', '2025-09-30'),
+(45, 3, '2025-04-01', '2025-05-29'),
+(7, 1, '2025-05-06', '2025-07-24'),
+(11, 2, '2025-03-01', '2025-07-27'),
+(4, 1, '2025-07-05', '2025-08-30'),
+(1, 3, '2025-10-01', '2025-12-21'),
+(2, 2, '2025-07-01', '2025-09-30'),
+(99, 2, '2025-02-14', '2025-04-27'),
+(47, 3, '2025-03-11', '2025-04-23');
+
+*/
+
+SELECT
+    pessoa.nome,
+    pessoa.cpf, 
+    curso.nome AS curso,
+    matricula.data_inicio,
+    matricula.data_termino
+FROM matricula
+INNER JOIN pessoa ON matricula.id_pessoa = pessoa.id 
+INNER JOIN curso ON matricula.id_curso = curso.id
+WHERE 
+    pessoa.sexo = 'masculino'
+ORDER BY
+    pessoa.id ASC;
+
+
+
+
+
