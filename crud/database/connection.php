2909524a0fa644a170f3b68a4c10a0232f7d1f27
José Coca <josecoca.cn@gmail.com>
2025-07-31 21:17:12 -0500

    altera modo de busca no banco de dados para retornar objetos e atualiza inserção de dados com novos campos

--- crud/database/connection.php
+++ crud/database/connection.php
@@ -3,7 +3,7 @@
 try {
     $connection = new PDO('mysql:host=localhost;dbname=senac;charset=utf8', 'root', '');
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
--- crud/list.php
+++ crud/list.php
@@ -1,5 +1,41 @@
 <?php include_once 'layout/header.php'; ?>
 
+<?php 
+    
+    require_once 'database/connection.php';
+
+    $select = $connection->prepare('SELECT * FROM pessoa');
+    $select->execute();
+
+?>
+
 <h1 class="mb-4">Listagem</h1>
+
+<div class="table-responsive">
+    <table class="table table-sm table-striped table-hover align-middle">
+        <thead class="table-light">
+            <tr>
+                <th>Nome</th>
+                <th>CPF</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Sexo</th>
+                <th>Data Nascimento</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($select->fetchAll() as $pessoa): ?>
+                <tr>
+                    <td><?= $pessoa->nome; ?></td>
+                    <td><?= $pessoa->cpf; ?></td>
+                    <td><?= $pessoa->email; ?></td>
+                    <td><?= $pessoa->telefone; ?></td>
+                    <td><?= $pessoa->sexo; ?></td>
+                    <td><?= $pessoa->data_nascimento; ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
 
 <?php include_once 'layout/footer.php'; ?>
